5b3d30711c9e116a9c48558ed29563dc7c62f547
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2023-02-16 19:59:17 +0600

    281A | Word Capitalization
    Problem Link: https://codeforces.com/problemset/problem/281/A

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,35 @@
+// Author: Rakin Sad Aftab (aftabrakinsad)
+// Date: 16-02-2023
+// Description: A. Word Capitalization
+// Status: Accepted
+// Time: 62 ms
+// Memory: 0 KB
+// Link: https://codeforces.com/problemset/problem/281/A
+// Platform: Codeforces
+// Version: 1.0.0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // input string
+    string s;
+    cin >> s;
+
+    // length of string
+    int l = s.length();
+
+    // converting first character to uppercase
+    for (int i = 0; i < l; i++)
+    {
+        if (islower(s[i]))
+            /*  Converting lowercase characters to uppercase  */
+            s[i] = s[i] - 32;
+        break;
+    }
+    // printing the string
+    cout << s;
+
+    return 0;
+}
