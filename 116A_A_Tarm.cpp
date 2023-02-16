e177ebc16ff86768f2f6d5b21ebb69005982d046
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2023-02-16 21:12:43 +0600

    116A | Tram
    Problem Link: https://codeforces.com/problemset/problem/116/A

---
+++ 116A_A_Tarm.cpp
@@ -0,0 +1,22 @@
+// Author: Rakin Sad Aftab (aftabrakinsad)
+// Date: 16-02-2023
+// Description: A Tram
+// Status: Not Done
+// Time: 0 ms
+// Memory: 0 KB
+// Link: https://codeforces.com/problemset/problem/116/A
+// Platform: Codeforces
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, a, b;
+    cin>> n;
+    for(int j = 0; j < n; j++)
+    {
+        cin>>a>>b;
+    }
+    return 0;
+}
