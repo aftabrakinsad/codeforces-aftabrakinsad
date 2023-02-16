a75945819144ae437fc9b135ac17c5cbe1866a58
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2023-02-16 21:13:58 +0600

    467A | George and Accommodation
    Problem Link: https://codeforces.com/problemset/problem/467/A

---
+++ 467A_George_and_Accommodation.cpp
@@ -0,0 +1,22 @@
+// Author: Rakin Sad Aftab (aftabrakinsad)
+// Date: 16-02-2023
+// Description: George and Accommodation
+// Status: Not Completed
+// Time: 0 ms
+// Memory: 0 KB
+// Link: https://codeforces.com/problemset/problem/467/A
+// Platform: Codeforces
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, pi, pa, count = 0, temp;
+    cin >> i;
+    for(int j = 0; j < i; j++)
+    {
+        cin >> pi >> pa;
+    }
+    return 0;
+}
