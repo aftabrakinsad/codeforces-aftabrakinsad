affd557a3a35df8e245f4518367a58bc2112fee9
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2023-02-16 20:00:31 +0600

    236A | Boy or Girl
    Problem Link: https://codeforces.com/problemset/problem/236/A

---
+++ A_Boy_or_Girl.cpp
@@ -0,0 +1,46 @@
+// Author: Rakin Sad Aftab (aftabrakinsad)
+// Date: 16-02-2023
+// Description: A Boy or Girl
+// Status: Accepted
+// Time: 62 ms
+// Memory: 0 KB
+// Link: https://codeforces.com/problemset/problem/236/A
+// Platform: Codeforces
+// Version: 1.0.0
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // variable declaration
+    int i, count = 0;
+    string input;
+    // input string
+    cin>>input;
+
+    // sorting the string
+    sort(input.begin(), input.end());
+    // length of string
+    int l = input.length();
+    // counting the number of distinct characters
+    for(i = 1; i < l; i++)
+    {
+        if(input[i] != input[i-1])
+        {
+            count++;
+        }
+    }
+
+    // printing the output
+    if(count & 1)
+    {
+        cout << "CHAT WITH HER!";
+    }
+    else
+    {
+        cout << "IGNORE HIM!";
+    }
+
+    return 0;
+}
